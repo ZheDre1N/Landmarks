65b62a4a8e3413329000a83167437888ec988c8a
Eugene Dudkin <ZheDre1N@gmail.com>
2023-10-09 17:16:16 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eugene Dudkin on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
